1c3e877dd48f9c9777519d766c9d61f842b57e62
elindsay <emma@quartz.co>
2022-02-13 17:36:40 -0800

    Basic toggling working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './assets/logo.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { Navbar, Nav } from 'react-bootstrap';
+import './App.scss';
+import { Navbar, Nav, Row, Col } from 'react-bootstrap';
 import { useState } from 'react';
+import StickerDesigner from './StickerDesigner';
 
 function App() {
   const [open, setOpen] = useState(false);
@@ -20,7 +20,7 @@ function App() {
           <Navbar.Toggle aria-controls="Left-nav" className="Hamburger" onClick={() => setOpen(!open)}/>
         </Navbar>
         { open &&
-          <Nav defaultActiveKey="/home" id="Left-nav" className="flex-column Left-nav">
+          <Nav defaultActiveKey="/home" id="Left-nav" className="flex-column Left-nav Sm-only">
             <Nav.Link className="Menu-item" href="/home">Home</Nav.Link>
             <Nav.Link className="Menu-item" eventKey="link-1">Design</Nav.Link>
             <Nav.Link className="Menu-item" eventKey="link-2">Stickers</Nav.Link>
@@ -28,13 +28,20 @@ function App() {
             <Nav.Link className="Menu-item" eventKey="link-2">Contact</Nav.Link>
           </Nav>
         }
-        <Nav defaultActiveKey="/home" id="Left-nav" className="flex-column Left-nav Lg-only">
-          <Nav.Link className="Menu-item" href="/home">Home</Nav.Link>
-          <Nav.Link className="Menu-item" eventKey="link-1">Design</Nav.Link>
-          <Nav.Link className="Menu-item" eventKey="link-2">Stickers</Nav.Link>
-          <Nav.Link className="Menu-item" eventKey="link-2">Catalog</Nav.Link>
-          <Nav.Link className="Menu-item" eventKey="link-2">Contact</Nav.Link>
-        </Nav>
+        <Row>
+          <Col sm={0} md={3}>
+            <Nav defaultActiveKey="/home" id="Left-nav" className="flex-column Left-nav Lg-only">
+              <Nav.Link className="Menu-item" href="/home">Home</Nav.Link>
+              <Nav.Link className="Menu-item" eventKey="link-1">Design</Nav.Link>
+              <Nav.Link className="Menu-item" eventKey="link-2">Stickers</Nav.Link>
+              <Nav.Link className="Menu-item" eventKey="link-2">Catalog</Nav.Link>
+              <Nav.Link className="Menu-item" eventKey="link-2">Contact</Nav.Link>
+            </Nav>
+          </Col>
+          <Col sm={12} md={9}>
+            <StickerDesigner />
+          </Col>
+        </Row>
       </div>
     </div>
   );
---
+++ src/StickerDesigner.js
@@ -0,0 +1,112 @@
+import React, { useState } from 'react'
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
+import ToggleButton from 'react-bootstrap/ToggleButton'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+
+import cat from './assets/component_images/bodies/cat-white.png';
+import dog from './assets/component_images/bodies/dog-white.png';
+import lion from './assets/component_images/bodies/lion-white.png';
+import bear from './assets/component_images/bodies/bear-white.png';
+import e1_l from './assets/component_images/eyes/eye1-left.png';
+import e1_r from './assets/component_images/eyes/eye1-right.png';
+import e2_l from './assets/component_images/eyes/eye2-left.png';
+import e2_r from './assets/component_images/eyes/eye2-right.png';
+import e3_l from './assets/component_images/eyes/eye3-left.png';
+import e3_r from './assets/component_images/eyes/eye3-right.png';
+import e4_l from './assets/component_images/eyes/eye4-left.png';
+import e4_r from './assets/component_images/eyes/eye4-right.png';
+import e5_l from './assets/component_images/eyes/eye5-left.png';
+import e5_r from './assets/component_images/eyes/eye5-right.png';
+import m1 from './assets/component_images/mouths/mouth1.png';
+import m2 from './assets/component_images/mouths/mouth2.png';
+import m3 from './assets/component_images/mouths/mouth3.png';
+import m4 from './assets/component_images/mouths/mouth4.png';
+import m5 from './assets/component_images/mouths/mouth5.png';
+import m6 from './assets/component_images/mouths/mouth6.png';
+import m7 from './assets/component_images/mouths/mouth7.png';
+
+const bodies = [cat, dog, lion, bear]
+const bodypos = [
+  {mouth_pos: [400, 350], eye_top: 250, eye_offset: 280},
+  {mouth_pos: [400, 325], eye_top: 275, eye_offset: 280},
+  {mouth_pos: [405, 320], eye_top: 275, eye_offset: 280},
+  {mouth_pos: [400, 350], eye_top: 290, eye_offset: 260}
+]
+
+
+const eyes = [[e1_l, e1_r], [e2_l, e2_r], [e3_l, e3_r], [e4_l, e4_r], [e5_l, e5_r]]
+const mouths = [m1, m2, m3, m4, m5, m6, m7]
+
+
+const StickerDesigner = () => {
+  const [bodyIndex, setBodyIndex] = useState(0)
+  const [eyesIndex, setEyesIndex] = useState(0)
+  const [mouthIndex, setMouthIndex] = useState(0)
+  const [editType, setEditType] = useState("");
+
+  return(
+    <div>
+      <h4>Design a Sticker!</h4>
+      <div className="editTypeSelector">
+        <ToggleButtonGroup type="radio" value={editType} name="editType" onChange={(val) => setEditType(val)}>
+          <ToggleButton id="tbg-btn-1" value={"animal"} onClick={ () => setBodyIndex((bodyIndex + 1)%bodies.length) } variant="outline-primary" className="border-2">
+            Animal
+          </ToggleButton>
+          <ToggleButton id="tbg-btn-2" value={"eyes"} onClick={ () => setEyesIndex((eyesIndex + 1)%eyes.length) } variant="outline-primary" className="border-2">
+             Eyes
+          </ToggleButton>
+          <ToggleButton id="tbg-btn-3" value={"mouth"} onClick={ () => setMouthIndex((mouthIndex + 1)%mouths.length) } variant="outline-primary" className="border-2">
+            Mouth
+          </ToggleButton>
+        </ToggleButtonGroup>
+      </div>
+      <div className="subeditTypeSelector">
+        { editType == "animal" &&
+          <ToggleButtonGroup type="radio" value={editType} name="editType" onChange={(val) => setEditType(val)}>
+            <ToggleButton id="tbg-btn-1" value={"animal"} onClick={ () => setBodyIndex((bodyIndex + 1)%bodies.length) } variant="outline-primary" className="border-2">
+              <FontAwesomeIcon icon={faArrowLeft} />
+            </ToggleButton>
+            <ToggleButton id="tbg-btn-1" value={"animal"} onClick={ () => setBodyIndex((bodyIndex + 1)%bodies.length) } variant="outline-primary" className="border-2">
+              <FontAwesomeIcon icon={faArrowRight} />
+            </ToggleButton>
+            <ToggleButton id="tbg-btn-2" value={"eyes"} onClick={ () => setEyesIndex((eyesIndex + 1)%eyes.length) } variant="outline-primary" className="border-2">
+               Color
+            </ToggleButton>
+          </ToggleButtonGroup>
+        }
+        { editType == "eyes" &&
+          <ToggleButtonGroup type="radio" value={editType} name="editType" onChange={(val) => setEditType(val)}>
+            <ToggleButton id="tbg-btn-1" value={"animal"} onClick={ () => setBodyIndex((bodyIndex + 1)%bodies.length) } variant="outline-primary" className="border-2">
+              <FontAwesomeIcon icon={faArrowLeft} />
+            </ToggleButton>
+            <ToggleButton id="tbg-btn-1" value={"animal"} onClick={ () => setBodyIndex((bodyIndex + 1)%bodies.length) } variant="outline-primary" className="border-2">
+              <FontAwesomeIcon icon={faArrowRight} />
+            </ToggleButton>
+            <ToggleButton id="tbg-btn-2" value={"eyes"} onClick={ () => setEyesIndex((eyesIndex + 1)%eyes.length) } variant="outline-primary" className="border-2">
+               Color
+            </ToggleButton>
+          </ToggleButtonGroup>
+        }
+        { editType == "mouth" &&
+          <ToggleButtonGroup type="radio" value={editType} name="editType" onChange={(val) => setEditType(val)}>
+            <ToggleButton id="tbg-btn-1" value={"animal"} onClick={ () => setBodyIndex((bodyIndex + 1)%bodies.length) } variant="outline-primary" className="border-2">
+              <FontAwesomeIcon icon={faArrowLeft} />
+            </ToggleButton>
+            <ToggleButton id="tbg-btn-1" value={"animal"} onClick={ () => setBodyIndex((bodyIndex + 1)%bodies.length) } variant="outline-primary" className="border-2">
+              <FontAwesomeIcon icon={faArrowRight} />
+            </ToggleButton>
+          </ToggleButtonGroup>
+        }
+      </div>
+      <div className="designer">
+        <img src={bodies[bodyIndex]}  className="body"/>
+        <img src={eyes[eyesIndex][0]}  className="eye left" style={{left: bodypos[bodyIndex]['eye_offset'], top: bodypos[bodyIndex]['eye_top']}}/>
+        <img src={eyes[eyesIndex][1]}  className="eye right" style={{right: bodypos[bodyIndex]['eye_offset'], top: bodypos[bodyIndex]['eye_top']}}/>
+        <img src={mouths[mouthIndex]}  className="mouth" style={{left: bodypos[bodyIndex]['mouth_pos'][0], top: bodypos[bodyIndex]['mouth_pos'][1]}} />
+      </div>
+    </div>
+  )
+}
+
+export default StickerDesigner;
