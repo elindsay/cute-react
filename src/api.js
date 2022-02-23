4682ff70e127f352b24f6cb513b453db108ab3a5
elindsay <emma@quartz.co>
2022-02-23 13:30:42 -0800

    Basic image loading from db working

--- src/api.js
+++ src/api.js
@@ -3,7 +3,9 @@ import axios from './utils/axios';
 const baseUrl = process.env.REACT_APP_URL;
 
 const api = {
-  getComponentImages: async () => await axios.get(baseUrl+'/components/')
+  getBodyComposites: async () => await axios.get(baseUrl+'/body_composites'),
+  getEyeComposites: async () => await axios.get(baseUrl+'/eye_composites'),
+  getMouthComponentImages: async () => await axios.get(baseUrl+'/components?c_type=mouth'),
 }
 
 export default api;
--- src/elements/StickerDesigner.js
+++ src/elements/StickerDesigner.js
@@ -5,53 +5,26 @@ import ToggleButton from 'react-bootstrap/ToggleButton'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
-import cat from '../assets/component_images/bodies/cat-white.png';
-import dog from '../assets/component_images/bodies/dog-white.png';
-import lion from '../assets/component_images/bodies/lion-white.png';
-import bear from '../assets/component_images/bodies/bear-white.png';
-import e1_l from '../assets/component_images/eyes/eye1-left.png';
-import e1_r from '../assets/component_images/eyes/eye1-right.png';
-import e2_l from '../assets/component_images/eyes/eye2-left.png';
-import e2_r from '../assets/component_images/eyes/eye2-right.png';
-import e3_l from '../assets/component_images/eyes/eye3-left.png';
-import e3_r from '../assets/component_images/eyes/eye3-right.png';
-import e4_l from '../assets/component_images/eyes/eye4-left.png';
-import e4_r from '../assets/component_images/eyes/eye4-right.png';
-import e5_l from '../assets/component_images/eyes/eye5-left.png';
-import e5_r from '../assets/component_images/eyes/eye5-right.png';
-import m1 from '../assets/component_images/mouths/mouth1.png';
-import m2 from '../assets/component_images/mouths/mouth2.png';
-import m3 from '../assets/component_images/mouths/mouth3.png';
-import m4 from '../assets/component_images/mouths/mouth4.png';
-import m5 from '../assets/component_images/mouths/mouth5.png';
-import m6 from '../assets/component_images/mouths/mouth6.png';
-import m7 from '../assets/component_images/mouths/mouth7.png';
-
-const bodies = [cat, dog, lion, bear]
-const bodypos = [
-  {mouth_pos: [400, 350], eye_top: 250, eye_offset: 280},
-  {mouth_pos: [400, 325], eye_top: 275, eye_offset: 280},
-  {mouth_pos: [405, 320], eye_top: 275, eye_offset: 280},
-  {mouth_pos: [400, 350], eye_top: 290, eye_offset: 260}
-]
-
-
-const eyes = [[e1_l, e1_r], [e2_l, e2_r], [e3_l, e3_r], [e4_l, e4_r], [e5_l, e5_r]]
-const mouths = [m1, m2, m3, m4, m5, m6, m7]
-
-
 const StickerDesigner = () => {
   const [bodyIndex, setBodyIndex] = useState(0)
+  const [bodies, setBodies] = useState([{layers: [{signed_url: ""}]}])
   const [eyesIndex, setEyesIndex] = useState(0)
+  const [eyes, setEyes] = useState([{layers: [{signed_url: ""}, {signed_url: ""}]}])
   const [mouthIndex, setMouthIndex] = useState(0)
+  const [mouths, setMouths] = useState([{signed_url: ""}])
   const [editType, setEditType] = useState("");
 
   useEffect(() => {
-    api.getComponentImages()
-      .then(result => {
-        console.log(result)
-      })
-  });
+    api.getEyeComposites().then((result) => {
+      setEyes(result.data)
+    })
+    api.getMouthComponentImages().then((result) => {
+      setMouths(result.data)
+    })
+    api.getBodyComposites().then((result) => {
+      setBodies(result.data)
+    })
+  }, []);
 
   return(
     <div>
@@ -108,10 +81,12 @@ const StickerDesigner = () => {
         }
       </div>
       <div className="designer">
-        <img src={bodies[bodyIndex]}  className="body"/>
-        <img src={eyes[eyesIndex][0]}  className="eye left" style={{left: bodypos[bodyIndex]['eye_offset'], top: bodypos[bodyIndex]['eye_top']}}/>
-        <img src={eyes[eyesIndex][1]}  className="eye right" style={{right: bodypos[bodyIndex]['eye_offset'], top: bodypos[bodyIndex]['eye_top']}}/>
-        <img src={mouths[mouthIndex]}  className="mouth" style={{left: bodypos[bodyIndex]['mouth_pos'][0], top: bodypos[bodyIndex]['mouth_pos'][1]}} />
+        {bodies[bodyIndex].layers.map((layer_data, ix) => {
+          return <img src={layer_data.signed_url}  className={ layer_data.layer >= 0 ? "body" : "body hidden"} key={"layer-"+layer_data.layer}/>
+        })}
+        <img src={eyes[eyesIndex].layers[0].signed_url}  className="eye left" style={{left: bodies[bodyIndex]['eye_spacing'], top: bodies[bodyIndex]['eye_top']}}/>
+        <img src={eyes[eyesIndex].layers[1].signed_url}  className="eye right" style={{right: bodies[bodyIndex]['eye_spacing'], top: bodies[bodyIndex]['eye_top']}}/>
+        <img src={mouths[mouthIndex].signed_url}  className="mouth" style={{left: bodies[bodyIndex]['mouth_x'], top: bodies[bodyIndex]['mouth_y']}} />
       </div>
     </div>
   )
